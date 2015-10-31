a07b7227cdebf60a98a88e7b84f133d5c5217a7d
tylernappy <tylernappy@gmail.com>
2015-10-30 22:42:04 -0400

    stable hardware

--- index.ino
+++ index.ino
@@ -8,6 +8,10 @@
 #define PIXEL_COUNT 48
 #define PIXEL_TYPE WS2812B
 
+int faceIndex = 0;
+int logoIndex = 0;
+int powerBarIndex = 0;
+
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(PIXEL_COUNT, PIXEL_PIN, PIXEL_TYPE);
 
 void setup() {
@@ -53,39 +57,43 @@ void initialize() {
 //SparkCoreFunction
 int updateFaces(String command) {
     int n = command.toInt(); //find amount of things tweeted in
-    int level = n%12; //find the actual amount of LEDs to light
-    float colorLevel = (float)n/12.0; //find the color
     int red;
     int green;
     int blue;
-    if (colorLevel<1) {
-        red = 0;
-        green = 0;
-        blue = 255;
-    } else if (colorLevel < 2 && colorLevel >= 1) {
-        red = 255;
-        green = 0;
-        blue = 255;
-    } else if (colorLevel < 3 && colorLevel >= 2) {
-        red = 255;
-        green = 0;
-        blue = 0;
-    } else {
-        red = 255;
-        green = 0;
-        blue = 0;
-    }
-
-    for(int i = 0; i < level; i++) {
-        strip.setPixelColor(i, strip.Color(red, green, blue));
+    float oldColorLevel;
+    float newColorLevel;
+    while(faceIndex < n) {
+        int led = faceIndex%12; //which LED to light up
+        newColorLevel = (float)faceIndex/12.0; //which color to set the LED to
+        int difference = (int)newColorLevel - (int)oldColorLevel;
+        if (difference > 0 ){
+            resetLEDsUp(0, 12); //run function to reset lights
+        }
+        //
+        if (newColorLevel<1) {
+            red = 0;
+            green = 0;
+            blue = 255;
+        } else if (newColorLevel < 2 && newColorLevel >= 1) {
+            red = 255;
+            green = 0;
+            blue = 255;
+        } else if (newColorLevel < 3 && newColorLevel >= 2) {
+            red = 255;
+            green = 0;
+            blue = 0;
+        } else {
+            red = 255;
+            green = 0;
+            blue = 0;
+        }
+        strip.setPixelColor(led, strip.Color(red, green, blue));
+        faceIndex++;
+        oldColorLevel = newColorLevel;
     }
     Serial.println("Faces ---------");
     Serial.print("n: ");
     Serial.println(n);
-    Serial.print("level: ");
-    Serial.println(level);
-    Serial.print("colorLevel: ");
-    Serial.println(colorLevel);
     Serial.print("red: ");
     Serial.print(red);
     Serial.print(", green: ");
@@ -100,38 +108,44 @@ int updateFaces(String command) {
 
 int updateLogos(String command) {
     int n = command.toInt(); //find amount of things tweeted in
-    int level = n%12; //find the actual amount of LEDs to light
-    float colorLevel = (float)n/12.0; //find the color
     int red;
     int green;
     int blue;
-    if (colorLevel<1) {
-        red = 0;
-        green = 255;
-        blue = 0;
-    } else if (colorLevel < 2 && colorLevel >= 1) {
-        red = 0;
-        green = 255;
-        blue = 255;
-    } else if (colorLevel < 3 && colorLevel >= 2) {
-        red = 0;
-        green = 0;
-        blue = 255;
-    } else {
-        red = 0;
-        green = 0;
-        blue = 255;
+    float oldColorLevel;
+    float newColorLevel;
+    while(logoIndex < n) {
+        int ledUnMapped = logoIndex%12; //which LED to light up
+        int led = map(ledUnMapped, 0, 11, 23, 12);
+        newColorLevel = (float)logoIndex/12.0; //which color to set the LED to
+        int difference = (int)newColorLevel - (int)oldColorLevel;
+        if (difference > 0 ){
+            resetLEDsDown(24, 11); //run function to reset lights
+        }
+        //
+        if (newColorLevel<1) {
+            red = 0;
+            green = 255;
+            blue = 0;
+        } else if (newColorLevel < 2 && newColorLevel >= 1) {
+            red = 0;
+            green = 255;
+            blue = 255;
+        } else if (newColorLevel < 3 && newColorLevel >= 2) {
+            red = 0;
+            green = 0;
+            blue = 255;
+        } else {
+            red = 0;
+            green = 0;
+            blue = 255;
+        }
+        strip.setPixelColor(led, strip.Color(red, green, blue));
+        logoIndex++;
+        oldColorLevel = newColorLevel;
     }
-    for(int i = 23; i > 23-level; i--) { //count backwards from level (11 is the total amount of LED's on the ring)
-        strip.setPixelColor(i, strip.Color(red, green, blue));
-    }
-    Serial.println("Logos ---------");
+    Serial.println("Faces ---------");
     Serial.print("n: ");
     Serial.println(n);
-    Serial.print("level: ");
-    Serial.println(level);
-    Serial.print("colorLevel: ");
-    Serial.println(colorLevel);
     Serial.print("red: ");
     Serial.print(red);
     Serial.print(", green: ");
@@ -146,40 +160,43 @@ int updateLogos(String command) {
 
 int updatePowerBar(String command) {
     int n = command.toInt(); //find amount of things tweeted in
-    int level = n%24; //find the actual amount of LEDs to light
-    float colorLevel = (float)n/24.0; //find the color
     int red;
     int green;
     int blue;
-    if (colorLevel<1) {
-        red = 255;
-        green = 0;
-        blue = 0;
-    } else if (colorLevel < 2 && colorLevel >= 1) {
-        red = 122;
-        green = 122;
-        blue = 122;
-    } else if (colorLevel < 3 && colorLevel >= 2) {
-        red = 255;
-        green = 0;
-        blue = 255;
-    } else {
-        red = 255;
-        green = 0;
-        blue = 255;
+    float oldColorLevel;
+    float newColorLevel;
+    while(powerBarIndex < n) {
+        int led = (powerBarIndex%24) + 24; //which LED to light up
+        newColorLevel = (float)powerBarIndex/24.0; //which color to set the LED to
+        int difference = (int)newColorLevel - (int)oldColorLevel;
+        if (difference > 0 ){
+            resetLEDsUp(24, 48); //run function to reset lights
+        }
+        //
+        if (newColorLevel<1) {
+            red = 255;
+            green = 0;
+            blue = 0;
+        } else if (newColorLevel < 2 && newColorLevel >= 1) {
+            red = 122;
+            green = 122;
+            blue = 122;
+        } else if (newColorLevel < 3 && newColorLevel >= 2) {
+            red = 255;
+            green = 0;
+            blue = 255;
+        } else {
+            red = 255;
+            green = 0;
+            blue = 255;
+        }
+        strip.setPixelColor(led, strip.Color(red, green, blue));
+        powerBarIndex++;
+        oldColorLevel = newColorLevel;
     }
-    for(int i = 24; i < (level + 24); i++) { //count backwards from level (11 is the total amount of LED's on the ring)
-        strip.setPixelColor(i, strip.Color(red, green, blue));
-    }
-
-
-    Serial.println("Power Bar ---------");
+    Serial.println("Faces ---------");
     Serial.print("n: ");
     Serial.println(n);
-    Serial.print("level: ");
-    Serial.println(level);
-    Serial.print("colorLevel: ");
-    Serial.println(colorLevel);
     Serial.print("red: ");
     Serial.print(red);
     Serial.print(", green: ");
@@ -190,6 +207,20 @@ int updatePowerBar(String command) {
     strip.show();
     delay(40);
     return 1;
+}
+
+void resetLEDsUp(int start, int stop) {
+   for(int i = start; i < stop; i++) {
+      strip.setPixelColor(i, strip.Color(0, 0, 0));
+    }
+    strip.show();
+}
+
+void resetLEDsDown(int start, int stop) {
+   for(int i = start; i > stop; i--) {
+      strip.setPixelColor(i, strip.Color(0, 0, 0));
+    }
+    strip.show();
 }
 
 int solidLights(int redValue, int blueValue, int greenValue) {
